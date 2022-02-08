7b0dea8d67cfb2ca940f78e6dd98991eb75b5657
Ralph Schuler <ralph.schuler@nyphon.de>
2022-02-08 21:38:34 +0100

    stash

--- .github/scripts/generate-changelog.sh
+++ .github/scripts/generate-changelog.sh
@@ -5,22 +5,22 @@
 # ./generate-changelog.sh
 
 previous_tag=0
-i=0
-for current_tag in $(git tag --sort=-creatordate); do
+index=0
+for current_tag in $(git tag --sort=-creatordate -l); do
 
-    if [ "$i" -ge "20" ]; then
+    if [ "$index" -ge "20" ]; then
         exit 0
     fi
 
     if [ "$previous_tag" != 0 ]; then
         tag_date=$(git log -1 --pretty=format:'%ad' --date=short ${previous_tag})
-        printf "## ${previous_tag} (${tag_date})\n\n"
-        git log ${current_tag}...${previous_tag} --pretty=format:"* [%h (%an) %s](${GITHUB_SERVER_URL}/${GITHUB_REPOSITORY}/commits/%H)%b" --reverse | grep -v Merge
+        printf "## ${previous_tag} (${tag_date})\n"
+        git log ${current_tag}...${previous_tag} --pretty=format:"* [%h (%an) %s](${GITHUB_SERVER_URL}/${GITHUB_REPOSITORY}/commits/%H)" --reverse | grep -v Merge
         printf "\n"
     fi
 
     previous_tag=${current_tag}
-    i = $i + 1
+    let "index++"
 done
 
 exit 0
